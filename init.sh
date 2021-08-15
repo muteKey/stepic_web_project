18bb5520692c83569d8a4e74ec0276f748ad688a
Kirill Ushkov <kirillushkov2010@gmail.com>
2021-08-15 15:12:07 +0300

    added check if folder exists

--- init.sh
+++ init.sh
@@ -1,5 +1,11 @@
+if [ ! -d "/home/box/web" ] 
+then
+	mkdir -p web/public/img
+	mkdir -p web/public/css
+	mkdir -p web/public/js
+	mkdir -p web/uploads
+	mkdir -p web/etc
+fi
+
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
-sudo /etc/init.d/mysql start
